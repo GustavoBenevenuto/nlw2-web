251b602e0022f09118e6bec34721c07301fd77f9
Gustavo Benevenuto <gustavo.benesilva@gmail.com>
2020-08-03 16:23:03 -0300

    Conclusão Landing

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+
+
+const TeacherList = () => {
+    return (
+        <div>
+           TeacherList
+        </div>
+    );
+}
+
+export default TeacherList;
